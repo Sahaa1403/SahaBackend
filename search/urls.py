532585c9f07bbec9f638c28fc0aa0c2cd9b5026a
Fan Wang <nimadorostkar97@gmail.com>
2025-02-15 19:31:47 +0330

    APIs and search data

--- search/urls.py
+++ search/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from search.views import Search
+from search.views import Search,MediaSearch,Answer
 from search.mongodb import check_mongodb_connection
 
 
 urlpatterns = [
     path("search", Search.as_view(), name="search"),
+    path("media-search", MediaSearch.as_view(), name="media-search"),
+    path("answer/<int:id>/", Answer.as_view(),name="answer"),
     path("check-mongodb-connection", check_mongodb_connection, name="check-mongodb-connection"),
 ]
--- search/views.py
+++ search/views.py
@@ -5,20 +5,144 @@ from rest_framework.permissions import AllowAny
 from search.models import SearchData
 from search.serializers import SearchDataSerializer
 
-
 class Search(APIView):
     permission_classes = [AllowAny]
-    def get(self, *args, **kwargs):
-        data = SearchData.objects()  # Get all MongoDB documents
-        serializer = SearchDataSerializer(data, many=True)
-        return Response(serializer.data)
-
     def post(self, *args, **kwargs):
-        serializer = SearchDataSerializer(data=self.request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            data = self.request.data
+            search = data['search']
+            # todo - save search
+            # todo - connect to AI and get data
+            # todo - response data
+
+            response_data = {
+                "keywords": {"اعتراض", "نظام ـ سلامت"},
+                "locations": {"ایران", "تهران", "خوزستان"},
+                "possibilities": [
+                    {"probability": "حقیقت", "percentage": 17.2},
+                    {"probability": "دروغ", "percentage": 16.5},
+                    {"probability": "فریب", "percentage": 36.6},
+                    {"probability": "سوءقصد", "percentage": 28.9}
+                ],
+                "websites": [
+                    {"website": "وبسایت‌های داخلی", "percentage": 60.4},
+                    {"website": "شبکه‌های اجتماعی خارجی", "percentage": 92.2},
+                    {"website": "وبسایت‌های خارجی", "percentage": 100},
+                    {"website": "شبکه‌های اجتماعی داخلی", "percentage": 34.1}
+                ],
+                "posts": [
+                    {
+                        "title": "تجمع اعتراضی پرستاران مقابل استانداری خوزستان",
+                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoBujouyZLFZC5nyf7TIUPqqEkNqLyXeabew&s",
+                        "text": "امروز (30 آبان)، جمعی از پرستاران خوزستان با تجمع مقابل استانداری خواستار رسیدگی به مطالبات قانونی خود شدند. این پرستاران اصلی‌ترین خواسته‌های خود را برقراری فوق العاده خاص با ضریب 3 و گنجاندن آن در حکم، اجرای دقیق تعرفه‌گذاری عادلانه، اجرای قانون مشاغل سخت و زیان‌آور، ممنوعیت اضافه‌کار اجباری و برقراری امنیت شغلی عنوان کردند.",
+                        "date": "June 7, 2021"
+                    },
+                    {
+                        "title": "تجمع اعتراضی پرستاران مقابل استانداری خوزستان",
+                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoBujouyZLFZC5nyf7TIUPqqEkNqLyXeabew&s",
+                        "text": "امروز (30 آبان)، جمعی از پرستاران خوزستان با تجمع مقابل استانداری خواستار رسیدگی به مطالبات قانونی خود شدند. این پرستاران اصلی‌ترین خواسته‌های خود را برقراری فوق العاده خاص با ضریب 3 و گنجاندن آن در حکم، اجرای دقیق تعرفه‌گذاری عادلانه، اجرای قانون مشاغل سخت و زیان‌آور، ممنوعیت اضافه‌کار اجباری و برقراری امنیت شغلی عنوان کردند.",
+                        "date": "June 7, 2021"
+                    }
+                ],
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
+        except:
+            return Response("Something went wrong please try again.", status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class MediaSearch(APIView):
+    permission_classes = [AllowAny]
+    def post(self, *args, **kwargs):
+        try:
+            data = self.request.data
+            search = data['search']
+            # todo - save search
+            # todo - connect to AI and get data
+            # todo - response data
+
+            response_data = {
+                "keywords": {"اعتراض", "نظام ـ سلامت"},
+                "locations": {"ایران", "تهران", "خوزستان"},
+                "possibilities": [
+                    {"probability": "حقیقت", "percentage": 17.2},
+                    {"probability": "دروغ", "percentage": 16.5},
+                    {"probability": "فریب", "percentage": 36.6},
+                    {"probability": "سوءقصد", "percentage": 28.9}
+                ],
+                "websites": [
+                    {"website": "وبسایت‌های داخلی", "percentage": 60.4},
+                    {"website": "شبکه‌های اجتماعی خارجی", "percentage": 92.2},
+                    {"website": "وبسایت‌های خارجی", "percentage": 100},
+                    {"website": "شبکه‌های اجتماعی داخلی", "percentage": 34.1}
+                ],
+                "posts": [
+                    {
+                        "title": "تجمع اعتراضی پرستاران مقابل استانداری خوزستان",
+                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoBujouyZLFZC5nyf7TIUPqqEkNqLyXeabew&s",
+                        "text": "امروز (30 آبان)، جمعی از پرستاران خوزستان با تجمع مقابل استانداری خواستار رسیدگی به مطالبات قانونی خود شدند. این پرستاران اصلی‌ترین خواسته‌های خود را برقراری فوق العاده خاص با ضریب 3 و گنجاندن آن در حکم، اجرای دقیق تعرفه‌گذاری عادلانه، اجرای قانون مشاغل سخت و زیان‌آور، ممنوعیت اضافه‌کار اجباری و برقراری امنیت شغلی عنوان کردند.",
+                        "date": "June 7, 2021"
+                    },
+                    {
+                        "title": "تجمع اعتراضی پرستاران مقابل استانداری خوزستان",
+                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoBujouyZLFZC5nyf7TIUPqqEkNqLyXeabew&s",
+                        "text": "امروز (30 آبان)، جمعی از پرستاران خوزستان با تجمع مقابل استانداری خواستار رسیدگی به مطالبات قانونی خود شدند. این پرستاران اصلی‌ترین خواسته‌های خود را برقراری فوق العاده خاص با ضریب 3 و گنجاندن آن در حکم، اجرای دقیق تعرفه‌گذاری عادلانه، اجرای قانون مشاغل سخت و زیان‌آور، ممنوعیت اضافه‌کار اجباری و برقراری امنیت شغلی عنوان کردند.",
+                        "date": "June 7, 2021"
+                    }
+                ],
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
+        except:
+            return Response("Something went wrong please try again.", status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class Answer(APIView):
+    permission_classes = [AllowAny]
+    def get(self, *args, **kwargs):
+        try:
+            #search = Search.objects.get(id=self.kwargs["id"])
+            # todo - connect to AI and get search object
+            # todo - response data
+
+            data = {
+                "keywords": {"اعتراض","نظام ـ سلامت"},
+                "locations": {"ایران","تهران","خوزستان"},
+                "possibilities": [
+                    {"probability": "حقیقت", "percentage": 17.2},
+                    {"probability": "دروغ", "percentage": 16.5},
+                    {"probability": "فریب", "percentage": 36.6},
+                    {"probability": "سوءقصد", "percentage": 28.9}
+                ],
+                "websites": [
+                    {"website": "وبسایت‌های داخلی", "percentage": 60.4},
+                    {"website": "شبکه‌های اجتماعی خارجی", "percentage": 92.2},
+                    {"website": "وبسایت‌های خارجی", "percentage": 100},
+                    {"website": "شبکه‌های اجتماعی داخلی", "percentage": 34.1}
+                ],
+                "posts": [
+                    {
+                        "title": "تجمع اعتراضی پرستاران مقابل استانداری خوزستان",
+                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoBujouyZLFZC5nyf7TIUPqqEkNqLyXeabew&s",
+                        "text": "امروز (30 آبان)، جمعی از پرستاران خوزستان با تجمع مقابل استانداری خواستار رسیدگی به مطالبات قانونی خود شدند. این پرستاران اصلی‌ترین خواسته‌های خود را برقراری فوق العاده خاص با ضریب 3 و گنجاندن آن در حکم، اجرای دقیق تعرفه‌گذاری عادلانه، اجرای قانون مشاغل سخت و زیان‌آور، ممنوعیت اضافه‌کار اجباری و برقراری امنیت شغلی عنوان کردند.",
+                        "date": "June 7, 2021"
+                    },
+                    {
+                        "title": "تجمع اعتراضی پرستاران مقابل استانداری خوزستان",
+                        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoBujouyZLFZC5nyf7TIUPqqEkNqLyXeabew&s",
+                        "text": "امروز (30 آبان)، جمعی از پرستاران خوزستان با تجمع مقابل استانداری خواستار رسیدگی به مطالبات قانونی خود شدند. این پرستاران اصلی‌ترین خواسته‌های خود را برقراری فوق العاده خاص با ضریب 3 و گنجاندن آن در حکم، اجرای دقیق تعرفه‌گذاری عادلانه، اجرای قانون مشاغل سخت و زیان‌آور، ممنوعیت اضافه‌کار اجباری و برقراری امنیت شغلی عنوان کردند.",
+                        "date": "June 7, 2021"
+                    }
+                ],
+            }
+
+            return Response(data, status=status.HTTP_200_OK)
+        except:
+            return Response("Something went wrong please try again.", status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 
@@ -28,4 +152,12 @@ class Search(APIView):
     # Create a data
     data = SearchData(text="nima", email="nima@example.com")
     data.save()
+    
+    
+    get method example:
+    
+    def get(self, *args, **kwargs):
+       data = SearchData.objects()  # Get all MongoDB documents
+       serializer = SearchDataSerializer(data, many=True)
+       return Response(serializer.data)
 """
