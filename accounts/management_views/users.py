e9e529cf5cf68d8baeec30bcf7348ae0cbaee2da
arashmjr <arash.mjr@gmail.com>
2025-06-07 23:58:25 +0330

    fix pagination of userList api

--- accounts/management_views/users.py
+++ accounts/management_views/users.py
@@ -1,3 +1,4 @@
+import math
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -14,6 +15,16 @@ class CustomPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        total_pages = math.ceil(self.page.paginator.count / self.get_page_size(self.request))
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'last_page': total_pages,
+            'results': data
+        })
 
 
 
